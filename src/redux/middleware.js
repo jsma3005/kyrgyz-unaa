e2b26c91289d8f70a6a4f38f77e8588e675bd31a
almazesh <meanwhilealmaz@gmail.com>
2021-03-15 13:18:48 +0600

    almaz-adaptive

--- src/redux/middleware.js
+++ src/redux/middleware.js
@@ -2,4 +2,6 @@ import createSagaMiddleware from 'redux-saga';
 
 export const sagaMiddleware = createSagaMiddleware();
 
-export default [sagaMiddleware];
+const sagaArray = [sagaMiddleware]
+
+export default sagaArray;
--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -4,10 +4,12 @@ import products from './products/productsReducer';
 import categories from './products/categoryReducer';
 import langs from './languages/languagesReducer';
 
-export default {
+const mainReducer = {
     news,
     singleNews,
     products,
     categories,
     langs
 }
+
+export default mainReducer
